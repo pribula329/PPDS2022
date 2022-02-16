5be9fb2ba481048822d58ab4762593664ba89c36
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-02-16 14:53:22 +0100

    feat: cviko version

--- main1.py
+++ main1.py
@@ -1,16 +1,24 @@
 from fei.ppds import Thread
-
+from collections import Counter
 
 class Shared():
-    pass
+    def __init__(self,size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * size
 
 
-def fnc_test(shared):
-    pass
+def test(shared):
+    while shared.counter < shared.end:
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
 
 
-shared = Shared(1_000_000)
+shared = Shared(10000000)
 
-t1 = Thread(fnc_test, shared)
-
+t1 = Thread(test, shared)
+t2 = Thread(test, shared)
 t1.join()
+t2.join()
+
+print(Counter(shared.elms).most_common())
