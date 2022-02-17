0f63581f72325fe6aea1bc33113a607f6681504a
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-02-17 14:42:48 +0100

    feat: added mutex and 2 other solution

--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
 from fei.ppds import Thread, Mutex
 from collections import Counter
 
+
 class Shared():
     def __init__(self, size, mutex):
         self.counter = 0
@@ -9,24 +10,29 @@ class Shared():
         self.mutex = mutex
 
 
-
 def test(shared):
     while True:
-        shared.mutex.lock()
         if shared.counter >= shared.end:
-            shared.mutex.unlock()
             break
-        shared.elms[shared.counter] += 1
+        if shared.counter==0:
+            shared.mutex.lock()
+            shared.elms[shared.counter] += 1
+            shared.mutex.unlock()
+
+        else:
+            shared.elms[shared.counter] += 1
         shared.counter += 1
-        shared.mutex.unlock()
+
 
 
 mutex = Mutex()
-shared = Shared(1000000, mutex)
+shared = Shared(10000000, mutex)
+
 
 t1 = Thread(test, shared)
 t2 = Thread(test, shared)
 t1.join()
 t2.join()
+
 
 print(Counter(shared.elms).most_common())
