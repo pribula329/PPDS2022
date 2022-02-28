8be1a034f5cb3ed5935728db024f1fc2a9788d33
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-02-28 09:06:41 +0100

    feat(semaphores): added semaphores to fibonnaci

--- fibonacci.py
+++ fibonacci.py
@@ -5,14 +5,22 @@ from fei.ppds import Thread, Semaphore, Mutex, print, Event
 
 def fibonacci(i):
     sleep(randint(1, 10)/10)
+    print("Start thread: " + str(i))
+    print("Wait thread:" + str(i))
+    semaphores[i].wait()
+    print("Rerun thread: " + str(i))
     fib[i+2] = fib[i+1] + fib[i]
+    semaphores[i+1].signal(1)
+    print("Semaphore send signal in thread " + str(i))
 
 
-THREADS = 10
-fib = [0] * (THREADS + 2)
+THREADS = 15
+fib = [0] * (THREADS+2) # +2 for first and second index [0,1]
 fib[1] = 1
-
+semaphores = [Semaphore(0) for x in range(THREADS+1)] # semaphores for thread +1 for exception out of range
+semaphores[0].signal(1)
 threads = [Thread(fibonacci, i) for i in range(THREADS)]
+
 [t.join() for t in threads]
 
 print(fib)
