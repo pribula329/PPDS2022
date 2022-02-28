0335298a323216d851221076530004254a23f888
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-02-28 09:31:07 +0100

    feat(SharedEvent): added class SharedEvent for use fibonacci with Event

--- fibonacci.py
+++ fibonacci.py
@@ -7,26 +7,45 @@ class SharedSemaphore:
     def __init__(self, size):
         self.fib = [0] * (size + 2)  # +2 for first and second index [0,1]
         self.fib[1] = 1
-        self.semaphores = [Semaphore(0) for x in
-                           range(size + 1)]  # semaphores for thread +1 for exception out of range
-        self.semaphores[0].signal(1)
+        self.object = [Semaphore(0) for x in
+                       range(size + 1)]  # semaphores for thread +1 for exception out of range
+        self.object[0].signal(1)
+
+
+class SharedEvent:
+    def __init__(self, size):
+        self.fib = [0] * (size + 2)  # +2 for first and second index [0,1]
+        self.fib[1] = 1
+        self.object = [Event() for x in
+                       range(size + 1)]  # events for thread +1 for exception out of range
+        self.object[0].signal()
 
 
 def fibonacci(shared, i):
     sleep(randint(1, 10) / 10)
     print("Start thread: " + str(i))
-    print("Wait thread:" + str(i))
-    shared.semaphores[i].wait()
+    print("Wait thread: " + str(i))
+    shared.object[i].wait()
     print("Rerun thread: " + str(i))
     shared.fib[i + 2] = shared.fib[i + 1] + shared.fib[i]
-    shared.semaphores[i + 1].signal(1)
-    print("Semaphore send signal in thread " + str(i))
+    shared.object[i + 1].signal()
+    print("Object send signal in thread " + str(i))
 
 
 THREADS = 15
+#use of semaphores
 sharedSemaphores = SharedSemaphore(THREADS)
 threads = [Thread(fibonacci, sharedSemaphores, i) for i in range(THREADS)]
-
+print("------Semaphores------")
 [t.join() for t in threads]
 
+#use of events
+sharedEvent = SharedEvent(THREADS)
+threadsEvent = [Thread(fibonacci, sharedEvent, i) for i in range(THREADS)]
+print("------Event------")
+[t1.join() for t1 in threadsEvent]
+
+print("------Semaphores------")
 print(sharedSemaphores.fib)
+print("------Event------")
+print(sharedEvent.fib)
