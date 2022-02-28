7da1bb7c358ae13b5e1288dc433e99d2fa48395b
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-02-28 09:19:48 +0100

    refactor(SharedSemaphores): refactor arrays of fib and semaphores to class SharedSemaphores

--- fibonacci.py
+++ fibonacci.py
@@ -3,24 +3,30 @@ from time import sleep
 from fei.ppds import Thread, Semaphore, Mutex, print, Event
 
 
-def fibonacci(i):
-    sleep(randint(1, 10)/10)
+class SharedSemaphore:
+    def __init__(self, size):
+        self.fib = [0] * (size + 2)  # +2 for first and second index [0,1]
+        self.fib[1] = 1
+        self.semaphores = [Semaphore(0) for x in
+                           range(size + 1)]  # semaphores for thread +1 for exception out of range
+        self.semaphores[0].signal(1)
+
+
+def fibonacci(shared, i):
+    sleep(randint(1, 10) / 10)
     print("Start thread: " + str(i))
     print("Wait thread:" + str(i))
-    semaphores[i].wait()
+    shared.semaphores[i].wait()
     print("Rerun thread: " + str(i))
-    fib[i+2] = fib[i+1] + fib[i]
-    semaphores[i+1].signal(1)
+    shared.fib[i + 2] = shared.fib[i + 1] + shared.fib[i]
+    shared.semaphores[i + 1].signal(1)
     print("Semaphore send signal in thread " + str(i))
 
 
 THREADS = 15
-fib = [0] * (THREADS+2) # +2 for first and second index [0,1]
-fib[1] = 1
-semaphores = [Semaphore(0) for x in range(THREADS+1)] # semaphores for thread +1 for exception out of range
-semaphores[0].signal(1)
-threads = [Thread(fibonacci, i) for i in range(THREADS)]
+sharedSemaphores = SharedSemaphore(THREADS)
+threads = [Thread(fibonacci, sharedSemaphores, i) for i in range(THREADS)]
 
 [t.join() for t in threads]
 
-print(fib)
+print(sharedSemaphores.fib)
