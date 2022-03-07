d4ebf995069137ca4d3941b70c0701148ffb2158
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-03-07 19:44:52 +0100

    feat: added graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
 from time import sleep
+from matplotlib import pyplot
 from fei.ppds import Mutex, Semaphore, Thread, print
 
 
@@ -15,8 +16,8 @@ class Shared():
 def producer(shared):
     while True:
         # production
-        sleep(randint(1, 10)/10)
-        print('Produce')
+        sleep(randint(1, 10)/100)
+        #print('Produce')
         # control free space in warehouse
         shared.free.wait()
         if shared.finished:
@@ -31,6 +32,8 @@ def producer(shared):
         shared.items.signal()
 
 
+
+
 def consumer(shared):
     while True:
         # stock control
@@ -40,34 +43,53 @@ def consumer(shared):
         # warehouse access
         shared.mutex.lock()
         # get item from warehouse
-        sleep(randint(1,10)/100)
-        print('Get item from warehouse')
+        sleep(randint(1,10)/1000)
+        #print('Get item from warehouse')
         # leave warehouse
         shared.mutex.unlock()
         # tell producent work !!!
         shared.free.signal()
-        # processing item
-        sleep(randint(1,10)/10)
 
 
-produce_time_experiment = [0.1, 0.15, 0.25, 0.35]
-produce_people_experiment = [1, 2, 3, 5, 8]
+
+
+produce_time_experiment = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
+produce_people_experiment = [1, 2, 3, 5, 8, 10]
+result_experiment = []
 for time in produce_time_experiment:
 
     for person in produce_people_experiment:
-
+        sum_person = 0
         for i in range(10):
 
             share = Shared(10)
             producers = [Thread(producer, share) for x in range(person)]
-            consumers = [Thread(consumer, share) for y in range(2)]
+            consumers = [Thread(consumer, share) for y in range(10)]
 
-            sleep(0.05)
+            sleep(0.1)
             share.finished = True
-            print("Wait")
             share.items.signal(100)
             share.free.signal(100)
             for t in producers + consumers:
                 t.join()
-            print("End")
+            #print("---------------")
+            item_sec = share.count / 0.1
+            sum_person += item_sec
+        print(person)
+        result_experiment.append((time, person, sum_person / 10))
+    print(time)
 
+
+fig = pyplot.figure()
+ax = pyplot.axes(projection="3d")
+x = [k[0] for k in result_experiment]
+y = [k[1] for k in result_experiment]
+z = [k[2] for k in result_experiment]
+
+ax.set_xlabel("Time production")
+ax.set_ylabel("Count producers")
+ax.set_zlabel("Count product for seconds")
+
+ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
+
+pyplot.show()
