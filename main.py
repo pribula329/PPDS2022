f3a4065f81e028b5ba8fdbbb9583215788512e97
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-03-12 13:48:59 +0100

    feat(LS): class LS added

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ from random import randint
 
 
 class LS:
-    pass
+    def __init__(self):
+        self.mutex = Mutex()
+        self.counter = 0
+
+    def lock(self, semaphore):
+        self.mutex.lock()
+        count = self.counter
+        self.counter += 1
+        if self.counter == 1:
+            semaphore.wait()
+        self.mutex.unlock()
+        return count
+
+    def unlock(self, semaphore):
+        self.mutex.lock()
+        self.counter -= 1
+        if self.counter == 0:
+            semaphore.signal()
+        self.mutex.unlock()
 
 
 class Shared:
