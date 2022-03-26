8af88768380b1fae71a8b646e240af8fa85f6356
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-03-26 11:27:51 +0100

    feat: added class Shared

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fei.ppds import Thread, print
+from fei.ppds import Thread, Mutex, Semaphore, print
 
 
 class Shared():
-    pass
+    def __init__(self):
+        self.mutex = Mutex()
+        self.oxygen = 0
+        self.hydrogen = 0
+        self.barrier = Barrier(3)
+        self.oxyQueue = Semaphore(0)
+        self.hydroQueue = Semaphore(0)
 
 
 def oxygen():
