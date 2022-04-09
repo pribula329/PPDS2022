b5ae15a4794c01767923fc23748d1c13d34bc453
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-09 13:46:05 +0200

    fix: create switch of coprogram

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-def coprogram1(count):
+def coprogram1(count, fnc):
+    next(fnc)
+
     for x in range(count):
-        print("Program 2 start 1")
+        print("Program 1 send 1")
+        fnc.send(1)
+        print("Program 1 send 2")
+        fnc.send(2)
         yield
-        print("Program 2 start 2")
-        yield
-        print("Program 2 start 3")
-        yield
+    fnc.close()
 
 
-def coprogram2(count):
+def coprogram2(count, fnc):
+    next(fnc)
+
     for x in range(count):
-        print("Program 2 start 1")
+        print("Program 2 send 1")
+        fnc.send(1)
+        print("Program 2 send 2")
+        fnc.send(2)
         yield
-        print("Program 2 start 2")
-        yield
-        print("Program 2 start 3")
-        yield
+    fnc.close()
 
 
 def count1():
@@ -23,6 +27,7 @@ def count1():
     try:
         while True:
             cnt += (yield)
+            print(f'Count of employer in count1 is {cnt}')
     except GeneratorExit:
         print(f'Coprogram 1 send {cnt} employer')
 
@@ -32,20 +37,30 @@ def count2():
     try:
         while True:
             cnt += (yield)
+            print(f'Count of employer in count2 is {cnt}')
     except GeneratorExit:
         print(f'Coprogram 2 send {cnt} employer')
 
 
 def main(c1, c2):
+
     for x in range(10):
         try:
+            print()
+            print(f"ETAPA {x} Program 1---------------")
             next(c1)
+
         except StopIteration:
+            print()
             print("Program 1 is killed")
         try:
+            print()
+            print(f"ETAPA {x} Program 2---------------")
             next(c2)
         except StopIteration:
+            print()
             print("Program 2 is killed")
 
-
-main(coprogram1(1), coprogram2(3))
+cnt1 = count1()
+cnt2 = count2()
+main(coprogram1(3, cnt1), coprogram2(5, cnt2))
