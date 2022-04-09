2e2ac901caeb2ad94b83f9c205336a329905a4da
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-09 12:32:38 +0200

    feat: create example

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+def coprogram1(count):
+    for x in range(count):
+        print("Program 2 start 1")
+        yield
+        print("Program 2 start 2")
+        yield
+        print("Program 2 start 3")
+        yield
 
-def coprogram1():
-    print("Program 1")
-    yield
 
+def coprogram2(count):
+    for x in range(count):
+        print("Program 2 start 1")
+        yield
+        print("Program 2 start 2")
+        yield
+        print("Program 2 start 3")
+        yield
 
-def coprogram2():
-    print("Program 2")
-    yield
 
 
 def main(c1, c2):
-    while True:
-        next(c1)
-        next(c2)
+    for x in range(10):
+        try:
+            next(c1)
+        except StopIteration:
+            print("Program 1 is killed")
+        try:
+            next(c2)
+        except StopIteration:
+            print("Program 2 is killed")
 
 
-main(coprogram1(), coprogram2())
+main(coprogram1(1), coprogram2(3))
