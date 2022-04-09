0e78f7f8be0e2f32e44ff9b0472775c659a1a607
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-09 13:57:00 +0200

    docs: added documentation of code

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def coprogram1(count, fnc):
-    next(fnc)
+"""
+Author: Lukáš Pribula
+Simulation of the switching coprograms
+"""
 
+
+def coprogram1(count, fnc):
+    """
+    Function of simulation send employer  to count function
+    :param count: count of loop
+    :param fnc: next function for coprogram
+    :return: yield to main function
+    """
+    next(fnc)
     for x in range(count):
         print("Program 1 send 1")
         fnc.send(1)
@@ -11,8 +22,13 @@ def coprogram1(count, fnc):
 
 
 def coprogram2(count, fnc):
+    """
+    Function of simulation send employer  to count function
+    :param count: count of loop
+    :param fnc: next function for coprogram
+    :return: yield to main function
+    """
     next(fnc)
-
     for x in range(count):
         print("Program 2 send 1")
         fnc.send(1)
@@ -23,6 +39,10 @@ def coprogram2(count, fnc):
 
 
 def count1():
+    """
+    Function for count employer
+    :return: none
+    """
     cnt = 0
     try:
         while True:
@@ -33,6 +53,10 @@ def count1():
 
 
 def count2():
+    """
+    Function for count employer
+    :return: none
+    """
     cnt = 0
     try:
         while True:
@@ -43,7 +67,12 @@ def count2():
 
 
 def main(c1, c2):
-
+    """
+    Function of example
+    :param c1: coprogram1
+    :param c2: coprogram2
+    :return: none
+    """
     for x in range(10):
         try:
             print()
@@ -60,6 +89,7 @@ def main(c1, c2):
         except StopIteration:
             print()
             print("Program 2 is killed")
+
 
 cnt1 = count1()
 cnt2 = count2()
