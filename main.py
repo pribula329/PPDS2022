9cc253087f61c0567ec629b0f42d3e01dfc225d3
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-15 12:34:25 +0200

    feat: create function cooking

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from time import sleep
 
-def cooking():
-    pass
+
+def cooking(cook):
+    for i in range(3):
+        print(f"Kuchar {cook} vari")
+        sleep(i/10)
+        print(f"Kuchar {cook} dovaril {i} jedlo")
+        yield
 
 
 def main():
