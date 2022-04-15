52d24f891a16b882b39b1d9d31c4ccaccd3fbca2
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-15 12:57:37 +0200

    feat: in function main create base example

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from time import sleep
+from time import sleep,time
 
 
 def cooking(cook):
-    for i in range(3):
+    for i in range(1,4):
         print(f"Kuchar {cook} vari")
-        sleep(i/10)
+        sleep(i/3)
         print(f"Kuchar {cook} dovaril {i} jedlo")
         yield
 
 
 def main():
-    pass
+    start_time = time()
+    cooks = ['Lukas', 'Peter', 'Erik']
+
+    for c in cooks:
+        cook = cooking(c)
+        for x in range(4):
+            try:
+                next(cook)
+            except StopIteration:
+                print(f"Kuchar {c} dovaril vsetky jedla\n")
+    cas = time() - start_time
+    print(f"Celkovy cas varenia: {cas:.1f}")
+
+main()
