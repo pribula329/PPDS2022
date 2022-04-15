5bea5d63daa437b8a9cab389b2e679b26f2ac9c3
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-15 13:08:57 +0200

    docs: added code documentation

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from time import sleep,time
+"""
+Author: Lukáš Pribula
+Simulation of cooking synchronous
+"""
+from time import sleep, time
 
 
 def cooking(cook):
-    for i in range(1,4):
+    """
+    Function of cooking
+    :param cook: name of cook
+    :return: none
+    """
+    for i in range(1, 4):
         print(f"Kuchar {cook} vari")
         sleep(i/3)
         print(f"Kuchar {cook} dovaril {i} jedlo")
@@ -10,6 +19,10 @@ def cooking(cook):
 
 
 def main():
+    """
+    Function for base simulation
+    :return: none
+    """
     start_time = time()
     cooks = ['Lukas', 'Peter', 'Erik']
 
@@ -22,5 +35,6 @@ def main():
                 print(f"Kuchar {c} dovaril vsetky jedla\n")
     cas = time() - start_time
     print(f"Celkovy cas varenia: {cas:.1f}")
+
 
 main()
--- main2.py
+++ main2.py
@@ -1,17 +1,30 @@
-from time import sleep,time
+"""
+Author: Lukáš Pribula
+Simulation of cooking asynchronous
+"""
+from time import sleep, time
 import asyncio
 
+
 async def cooking(cook):
-    for i in range(1,4):
+    """
+    Function of cooking
+    :param cook: name of cook
+    :return: none
+    """
+    for i in range(1, 4):
         print(f"Kuchar {cook} vari")
         await asyncio.sleep(i/3)
         print(f"Kuchar {cook} dovaril {i} jedlo")
 
 
-
 async def main():
+    """
+    Function for base simulation
+    :return: none
+    """
     start_time = time()
-    await asyncio.gather(cooking('Lukas'),cooking('Peter'), cooking('Erik'))
+    await asyncio.gather(cooking('Lukas'), cooking('Peter'), cooking('Erik'))
     cas = time() - start_time
     print(f"Celkovy cas varenia: {cas:.1f}")
 
