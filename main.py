ab500397ebe8e104cb8dd5e9844e315b112aa289
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-23 13:20:57 +0200

    feat: add main function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from numba import cuda
 import numpy as np
+from PIL import Image
 import math
 
 
@@ -9,4 +10,21 @@ def rotuj(image1, vystup):
 
 
 def main():
-    pass
+    image = np.array("mini.png")
+    print(image.shape)
+    threadsperblock = (256, 256, 4)
+    blockX = math.ceil(image.shape[0] / threadsperblock[0])
+    blockY = math.ceil(image.shape[1] / threadsperblock[1])
+    blockZ = math.ceil(image.shape[2] / threadsperblock[2])
+    blockspergrid = (blockX, blockY, blockZ)
+
+    input1 = cuda.to_device(image)
+    output = cuda.device_array(image.shape)
+
+    rotuj[blockspergrid, threadsperblock](input1, output)
+
+    out = output.to_host()
+    out = Image.fromarray(out)
+    out.show()
+
+main()
