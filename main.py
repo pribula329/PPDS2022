a5e761faedcdfeb8d7a1738a1ce03bcb9cf5e86e
pribula329 <72249242+pribula329@users.noreply.github.com>
2022-04-23 13:23:08 +0200

    feat: add rotuj function

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import math
 
 @cuda.jit
 def rotuj(image1, vystup):
-    pass
+    x, y, z = cuda.grid(3)
+    if x < image1.shape[0] and y < image1.shape[1] and z < image1.shape[2]:
+        vystup[x][y][z] += (image1[127 - x][y][z])
+        print(x)
 
 
 def main():
-    image = np.array("mini.png")
+    image = np.array(Image.open("mini.png"))
     print(image.shape)
-    threadsperblock = (256, 256, 4)
+    threadsperblock = (128, 128, 4)
     blockX = math.ceil(image.shape[0] / threadsperblock[0])
     blockY = math.ceil(image.shape[1] / threadsperblock[1])
     blockZ = math.ceil(image.shape[2] / threadsperblock[2])
